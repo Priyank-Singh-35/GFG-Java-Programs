b480cc8b4596f197d963427c28890f37ef4d8d63
PRIYANK SINGH <priyanksingh5004@gmail.com>
2024-07-24 17:59:36 +0530

    Create InsertAtEnd.java

---
+++ Arrays/InsertAtEnd.java
@@ -0,0 +1,7 @@
+class Insert {
+    // Function to insert an element at the end of the array
+    void insertAtEnd(int[] arr, int sizeOfArray, int element) {
+        // Insert the element at the last position of the array
+        arr[sizeOfArray - 1] = element;
+    }
+}
